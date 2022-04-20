5934310b2b989b05ae8de94ff5ed1ff9c2392b3c
Mitali <mitalij194@gmail.com>
2022-04-20 12:41:24 +0530

    day 10 completed

--- lib/Pages/home_page.dart
+++ lib/Pages/home_page.dart
@@ -1,21 +1,28 @@
+import 'package:catalog_app/models/catalog.dart';
 import 'package:catalog_app/widgets/drawer.dart';
+import 'package:catalog_app/widgets/item_widgets.dart';
 import 'package:flutter/material.dart';
 class Homepage extends StatelessWidget {
-  final int days = 30;
+  final int days = 30;                                // elements are mutable widgets are immutable
   
 
   @override
-  Widget build(BuildContext context) {
-    
+  Widget build(BuildContext context) {          // context is stateless element 
+    final dummylist = List.generate(50, (index) => CatalogModel.items[0]);
     return Scaffold(
       appBar: AppBar(
         title: const Text("Catalog App"),
         
       ),
       
-      body: Center(
-        child: Container(
-          child: const Text("welcome 30 days of flutter"),
+      body: Padding(
+        padding: const EdgeInsets.all(18.0),
+        child: ListView.builder(              // listbuilder is used to render the element in screen one by one in scrollable view 
+          itemCount: dummylist.length,
+          itemBuilder: (context, index) {
+            return ItemWidget(item: dummylist[index]) ;
+            
+          },
         ),
       ),
       drawer: const MyDrawer(),
---
+++ lib/widgets/item_widgets.dart
@@ -0,0 +1,32 @@
+import 'package:catalog_app/models/catalog.dart';
+import 'package:flutter/material.dart';
+class ItemWidget extends StatelessWidget {
+  final Item item;
+
+  const ItemWidget({Key? key, required this.item})
+  : assert (item != null ),       // assert only use in debugging tym not in production time
+    super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        onTap: () {
+          print("${item.name} pressed");
+        },
+        leading: Image.network(item.image),
+        title: Text(item.name),
+        subtitle: Text(item.desc),
+        trailing: Text("\$${item.price}",
+        textScaleFactor: 1.6,
+        style: const TextStyle(color: Colors.blue,
+        fontWeight: FontWeight.bold,
+        ),
+        
+        ),
+        
+        
+      ),
+    );
+  }
+}
